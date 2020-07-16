7e74a4e02e19d3d5263d7ee8bcdeb1f1788cc3e4
Onur Bakan <46714059+onurbakan@users.noreply.github.com>
2020-07-16 04:32:30 +0300

    Reskinning the Game

--- Assets/Scripts/CameraScaler.cs
+++ Assets/Scripts/CameraScaler.cs
@@ -10,6 +10,8 @@ public class CameraScaler : MonoBehaviour
     public float cameraOffset;
     public float aspectRatio = 0.625f;
     public float padding = 2;
+    public float yOffset = 1;
+
 
 
 
@@ -25,7 +27,7 @@ public class CameraScaler : MonoBehaviour
 
     void RepositionCamera(float x, float y)
     {
-        Vector3 tempPosition = new Vector3(x / 2, y / 2, cameraOffset);
+        Vector3 tempPosition = new Vector3(x / 2, y / 2 + yOffset, cameraOffset);
         transform.position = tempPosition;
         if (board.width >= board.height)
         {
