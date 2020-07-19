5f701d9dbbdc5dc0879f1b7f8304df633522d7b0
Onur Bakan <46714059+onurbakan@users.noreply.github.com>
2020-07-19 06:03:01 +0300

    Score Bar and Score Goals

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -49,6 +49,7 @@ public class Board : MonoBehaviour
     private int streakValue = 1;
     private ScoreManager scoreManager;
     public float refillDelay = 0.5f;
+    public int[] scoreGoals;
 
 
 
--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -6,13 +6,16 @@ using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
+    private Board board;
     public TextMeshProUGUI scoreText;
     public int score;
+    public Image scoreBar;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        board = FindObjectOfType<Board>();
+        UpdateBar();
     }
 
     // Update is called once per frame
@@ -24,5 +27,15 @@ public class ScoreManager : MonoBehaviour
     public void IncreaseScore(int amountToIncrease)
     {
         score += amountToIncrease;
+        UpdateBar();
+    }
+
+    private void UpdateBar()
+    {
+        if (board != null && scoreBar != null)
+        {
+            int length = board.scoreGoals.Length;
+            scoreBar.fillAmount = (float)score / (float)board.scoreGoals[length - 1];
+        }
     }
 }
