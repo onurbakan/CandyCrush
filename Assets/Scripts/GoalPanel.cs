aba0dff4263f869316da2d6d23bc0d9769dd76ff
Onur Bakan <46714059+onurbakan@users.noreply.github.com>
2020-07-22 00:35:07 +0300

    Level Goals

---
+++ Assets/Scripts/GoalPanel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class GoalPanel : MonoBehaviour
+{
+
+    public Image thisImage;
+    public Sprite thisSprite;
+    public TextMeshProUGUI thisText;
+    public string thisString;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Setup();
+    }
+
+    void Setup()
+    {
+        thisImage.sprite = thisSprite;
+        thisText.text = thisString;
+    }
+}
