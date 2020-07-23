c76caa6f1cdc56e9454e3405d47ae8672b08ef88
Onur Bakan <46714059+onurbakan@users.noreply.github.com>
2020-07-23 06:28:58 +0300

    Level Goals part 2

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -49,6 +49,7 @@ public class Board : MonoBehaviour
     private int streakValue = 1;
     private ScoreManager scoreManager;
     private SoundManager soundManager;
+    private GoalManager goalManager;
     public float refillDelay = 0.5f;
     public int[] scoreGoals;
 
@@ -57,6 +58,7 @@ public class Board : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        goalManager = FindObjectOfType<GoalManager>();
         soundManager = FindObjectOfType<SoundManager>();
         scoreManager = FindObjectOfType<ScoreManager>();
         breakableTiles = new BackgroundTile[width, height];
@@ -304,6 +306,13 @@ public class Board : MonoBehaviour
                     breakableTiles[column, row] = null;
                 }
             }
+
+            if (goalManager != null)
+            {
+                goalManager.CompareGoal(allDots[column, row].tag.ToString());
+                goalManager.UpdateGoals();
+            }
+
             //Does the sound manager exist?
             if (soundManager != null)
             {
--- Assets/Scripts/GoalManager.cs
+++ Assets/Scripts/GoalManager.cs
@@ -15,6 +15,7 @@ public class GoalManager : MonoBehaviour
 {
 
     public BlankGoal[] levelGoals;
+    public List<GoalPanel> currentGoals = new List<GoalPanel>();
     public GameObject goalPrefab;
     public GameObject goalIntroParent;
     public GameObject goalGameParent;
@@ -22,10 +23,10 @@ public class GoalManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        SetupIntroGoals();
+        SetupGoals();
     }
 
-    void SetupIntroGoals()
+    void SetupGoals()
     {
         for (int i = 0; i < levelGoals.Length; i++)
         {
@@ -36,18 +37,43 @@ public class GoalManager : MonoBehaviour
             GoalPanel panel = goal.GetComponent<GoalPanel>();
             panel.thisSprite = levelGoals[i].goalSprite;
             panel.thisString = "0/" + levelGoals[i].numberNeeded;
+
             //Create a new Goal Panel at the goalGameParent position
             GameObject gameGoal = Instantiate(goalPrefab, goalGameParent.transform.position, Quaternion.identity);
             gameGoal.transform.SetParent(goalGameParent.transform);
             panel = gameGoal.GetComponent<GoalPanel>();
+            currentGoals.Add(panel);
             panel.thisSprite = levelGoals[i].goalSprite;
             panel.thisString = "0/" + levelGoals[i].numberNeeded;
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UpdateGoals()
     {
-        
+        int goalsCompleted = 0;
+        for (int i = 0; i < levelGoals.Length; i++)
+        {
+            currentGoals[i].thisText.text = "" + levelGoals[i].numberCollected + "/" + levelGoals[i].numberNeeded;
+            if (levelGoals[i].numberCollected >= levelGoals[i].numberNeeded)
+            {
+                goalsCompleted++;
+                currentGoals[i].thisText.text = "" + levelGoals[i].numberNeeded + "/" + levelGoals[i].numberNeeded;
+            }
+        }
+        if (goalsCompleted >= levelGoals.Length)
+        {
+            Debug.Log("You win!");
+        }
+    }
+
+    public void CompareGoal(string goalToCompare)
+    {
+        for (int i = 0; i < levelGoals.Length; i++)
+        {
+            if (goalToCompare == levelGoals[i].matchValue)
+            {
+                levelGoals[i].numberCollected++;
+            }
+        }
     }
 }
